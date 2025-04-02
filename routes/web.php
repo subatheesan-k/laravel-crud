bac26b8558a4a9c361d55a98ca21a011c2f6a592
subatheesan-k <subatheesandev@gmail.com>
2025-04-02 23:42:48 +0530

    validation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('customer.index');
+})->name('home');
+
+Route::resource('customer', CustomerController::class);
